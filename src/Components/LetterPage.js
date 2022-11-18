3c02e382d4a6710d27e1eb15e5373983890b3c58
jpark0506 <peterp1@vscode-server.asia-northeast3-a.c.northern-timer-364315.internal>
2022-11-18 13:57:08 +0000

    added jpg

--- src/Components/LetterPage.js
+++ src/Components/LetterPage.js
@@ -112,7 +112,7 @@ const LetterPage = ({page,incrNum}) => {
               {
                   Data.Photos[page] !== 0 ? 
                  
-                 <ImageContainer src={`/img/${page-20}`}>
+                 <ImageContainer src={`/img/${page-20}.jpg`}>
                   </ImageContainer>:<div></div>
                   
               }
